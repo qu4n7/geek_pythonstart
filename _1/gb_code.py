af7ce1e47c53a1d68091be43d7008108ba79339e
qu4n7 <13035@protonmail.com>
2019-03-26 14:17:47 +0300

    задание к 1 уроку

---
+++ _1/gb_code.py
@@ -0,0 +1,128 @@
+### НЕ ДЕЛАЕМ ДЗ ОТ GeekBrains!!! ###
+
+# 1. https://www.python.org/
+# 2. https://www.jetbrains.com/pycharm/download/#section=windows (COMMUNITY!!!!!)
+
+# print('Hello world')
+# print(139861986926834 + 200)
+
+# number1 = 5
+# number2 = 2
+
+# PEP-8
+# Ctrl(CMD) + Alt + L  форматирование кода
+# Ctrl(CMD) + /  комментирование кода
+# full_name = 'Sizov Sergey'
+
+# print(number1 + number2)
+# print(number1 - number2)
+# print(number1 * number2)
+# print(number1 / number2)
+# print(number1 // number2)
+# print(number1 % number2)
+# print(number1 ** number2)
+
+# a = 100  # int
+# name = 'Oleg' # str
+# surname = "Petrov" # str
+# money = 200.10 # float
+
+# bool / Булев тип  # bool
+# is_admin = True
+# is_admin = False
+#
+# peoples = [100, 'asdas', money] # list
+# print(peoples[2])
+# peoples.append(200)
+# not_edit_peoples = (100, 'asdas', money) # tuple
+# human = {'name': 'Ivan', 'age': 30, 'data': [1, 2, 3, 4, 5]} # dict
+
+
+# print('100' + '1') # Слияние строк, конкатенация строк, склеивание.
+
+# x = '1'  # str
+# print(100 + int(x))
+# print(str(100))
+# print(float(100))
+# print(int(10.92))
+
+# name = input('Ваше имя:') # Input всегда возвращает str!!!
+# print('Привет', name)
+
+# age = int(input('Возраст:'))
+# print(age + 10)
+# print(age + 10)
+# print(age + 10)
+# print(age + 10)
+# print(age + 10)
+# print(age + 10)
+# print(age + 10)
+# print(age + 10)
+# print(age + 10)
+
+# print(5 > 1)
+# print(5 < 1)
+# print(5 == 1)
+# print(5 != 1)
+# print(5 >= 1)
+# print(5 <= 1)
+
+# print(bool(1))
+# print(bool(0))
+# print(bool(100000))
+# print(bool(-20))
+# print(bool('asdasd'))
+# print(bool(''))
+
+# price = 100
+# money = int(input('Сколько у вас денег:'))
+#
+# if money >= price:
+#     print('Мы можем это купить!')
+# elif price - money <= 5:
+#     print('Можем поторговаться')
+# elif price - money <= 10:
+#     print('Поищу в зимней куртке..')
+# else:
+#     print('У нас нехватает денег...')
+
+# name = input('Name:')
+# surname = input('Surname')
+#
+# if name and surname:
+#     print('Вы ввели какое-то имя.')
+#     if name == 'Oleg':
+#         print('Привет Олег!')
+# elif name or surname:
+#     print('Вы не ввели имя либо фамилию!')
+# else:
+#     print('Вы ничего не ввели!')
+
+# number = int(input('Введите число от 1 до 5: '))
+#
+# while number < 1 or number > 5:
+#     print('Неправильно, попробуйте еще раз!')
+#     number = int(input('Введите число от 1 до 5'))
+#
+#
+# print('Вы правильно ввели число!')
+
+# x = 0
+#
+# while x < 10:
+#     # if x == 8:
+#     #     break
+#     # elif x == 4:
+#     #     x += 1
+#     #     continue
+#
+#     print(x)
+#     # x = x + 1
+#     x += 1
+# else:
+#     print('Цикл завершился по условию!')
+
+
+# Easy, Normal, Hard - pastebin.com
+# НЕ ДЕЛАЕМ ДЗ ОТ GeekBrains!!!
+# GIT - обязательно посмотрите урок, связанный со сдачей ДЗ.
